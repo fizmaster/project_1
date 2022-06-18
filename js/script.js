b38ebf7cfcaf1e3ac15d15411f3c6b9402e4d4e7
Kira <example@gmail.com>
2022-06-18 18:00:49 +0400

    first comit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 +  "5");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+    
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(8%3);
+
+    console.log(2 + 2 * 2 !== 6);
+
+    const isCheked = false,
+    isClose = false;
+
+    console.log(!isCheked || !isClose);

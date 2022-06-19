8ed3cee3d1e2cb8ad2df7dc24881789c2bd1cf5c
Kira <example@gmail.com>
2022-06-19 19:30:28 +0400

    my last update 19.06

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 "use strict";
 
-console.log('arr' + " - object");
-console.log(4 +  "5");
 
-let incr = 10,
-    decr = 10;
 
-    // ++incr;
-    // --decr;
-    
-    console.log(++incr);
-    console.log(--decr);
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
-    console.log(8%3);
 
-    console.log(2 + 2 * 2 !== 6);
 
-    const isCheked = false,
-    isClose = false;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-    console.log(!isCheked || !isClose);
+const a = prompt('Один из последних фильмов', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних фильмов', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);

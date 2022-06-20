8d54c971b9ed2a60e68528caf590a8f4644fb050
Kira <example@gmail.com>
2022-06-20 23:34:19 +0400

    new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -20,28 +20,30 @@ const personalMovieDB = {
 // personalMovieDB.movies [c] = d;
 
 
-// for (let i = 0; i < 2; i++){
-//     const a = prompt('Один из последних фильмов', '');
-//     const b = prompt('На сколько оцените его?', '');
-
-// if (a != null && b !=null && a != '' && b != '' && a.length < 50) {
-//     personalMovieDB.movies [a] = b;
-//     console.log('done');
-// } else {
-//     console.log('Error');
-//     i--;
-// }
-
-// }
-
-let num = 0;
-
-do {
-    num++;
+for (let i = 0; i < 2; i++){
     const a = prompt('Один из последних фильмов', '');
     const b = prompt('На сколько оцените его?', '');
+
+if (a != null && b !=null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies [a] = b;
+    console.log('done');
+} else {
+    console.log('Error');
+    i--;
 }
-while (num < 2);
+
+}
+
+
+// let num = 0;
+
+// do {
+    
+//     num++;
+//     const a = prompt('Один из последних фильмов', '');
+//     const b = prompt('На сколько оцените его?', '');
+// }
+// while (num < 2);
 
 
 console.log(personalMovieDB);
@@ -51,23 +53,33 @@ console.log(personalMovieDB);
 // console.log('Вы не указали, сколько фильмов вы посмотрели. Введите заново.');
 // const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 // }
-
-if (personalMovieDB.count < 10) {
+ if (personalMovieDB.count == "") {
+    alert('Ты не ввел ничего');
+    console.log('Ты не ввел ничего');
+    do {
+        const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+    }
+    while (personalMovieDB.count < 0);
+} 
+else if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
 alert('Просмотрено довольно мало фильмов');
 console.log('Просмотрено довольно мало фильмов');
 }
 
-else if (30 < personalMovieDB.count) {
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+    console.log('Вы классический зритель');
+} 
+else if (personalMovieDB.count >= 30) {
     alert('Вы киноман');
     console.log('Вы киноман');
 } 
 
-else if (10 <= personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-    console.log('Вы классический зритель');
+
+else {
+    alert('Хотя бы один глянул бы');
+    console.log('Хотя бы один глянул бы');
 }
-
-
 
    
 
